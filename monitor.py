ce19e90771d44643c1913ab5f24c197dda88aa80
andresmendoza24 <andresmendoza24@yahoo.es>
2026-06-08 16:48:55 -0500

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -4,25 +4,14 @@ import requests
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-def enviar_mensaje(texto):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    requests.post(url, data={
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+respuesta = requests.post(
+    url,
+    data={
         "chat_id": CHAT_ID,
-        "text": texto
-    })
-
-btc = requests.get(
-    "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
-).json()
-
-eth = requests.get(
-    "https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT"
-).json()
-
-mensaje = (
-    f"📊 Monitoreo Cripto\n\n"
-    f"BTC: ${float(btc['price']):,.2f}\n"
-    f"ETH: ${float(eth['price']):,.2f}"
+        "text": "🚀 Prueba exitosa desde GitHub Actions"
+    }
 )
 
-enviar_mensaje(mensaje)
+print(respuesta.text)
