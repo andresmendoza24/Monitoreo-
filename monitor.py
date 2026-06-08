10e236683f54b78ed793956ac098de44bc5bf3c5
andresmendoza24 <andresmendoza24@yahoo.es>
2026-06-08 17:09:38 -0500

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,21 +1,6 @@
-import os
-import requests
-
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-
-print("TOKEN existe:", TOKEN is not None)
-print("CHAT_ID:", CHAT_ID)
-
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-
-respuesta = requests.post(
-    url,
-    data={
-        "chat_id": CHAT_ID,
-        "text": "🚀 Mensaje de prueba desde GitHub"
-    }
-)
-
-print("Codigo:", respuesta.status_code)
-print("Respuesta:", respuesta.text)
+      - name: Ejecutar monitor
+        env:
+          TELEGRAM_TOKEN: ${{ secrets.TELEGRAM_TOKEN }}
+          TELEGRAM_CHAT_ID: ${{ secrets.TELEGRAM_CHAT_ID }}
+        run: |
+          python monitor.py
